c78bb2b9ff71e0e19d96e8af7b13f06835bdd93b
Hubert Kuszlik <hubertkuszlik1@gmail.com>
2025-04-26 23:52:17 +0200

    js functions added

--- js/script.js
+++ js/script.js
@@ -1,101 +1,148 @@
 // Kalkulator:
-{
-const formElement = document.querySelector(".js-form");
-const changeElement = document.querySelector(".js-change");
-const currencyElement = document.querySelector(".js-currency")
-const sumElement = document.querySelector(".js-sum");
-const exchangeElement = document.querySelector(".js-exchange");
+// {
+// const formElement = document.querySelector(".js-form");
+// const changeElement = document.querySelector(".js-change");
+// const currencyElement = document.querySelector(".js-currency")
+// const sumElement = document.querySelector(".js-sum");
+// const exchangeElement = document.querySelector(".js-exchange");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+// formElement.addEventListener("submit", (event) => {
+//     event.preventDefault();
 
-    if (currencyElement.value === ("EUR")) {
-        sum = (changeElement.value / 4.28);
-        exchange = (4.28);
-        symbol = ("€");
+//     if (currencyElement.value === ("EUR")) {
+//         sum = (changeElement.value / 4.28);
+//         exchange = (4.28);
+//         symbol = ("€");
 
-    };
-    if (currencyElement.value === ("GBP")) {
-        sum = (changeElement.value / 4.99);
-        exchange = (4.99);
-        symbol = ("£");
+//     };
+//     if (currencyElement.value === ("GBP")) {
+//         sum = (changeElement.value / 4.99);
+//         exchange = (4.99);
+//         symbol = ("£");
 
-    };
+//     };
 
-    if (currencyElement.value === ("USD")) {
-        sum = (changeElement.value / 3.76);
-        exchange = (3.76);
-        symbol = ("USD");
+//     if (currencyElement.value === ("USD")) {
+//         sum = (changeElement.value / 3.76);
+//         exchange = (3.76);
+//         symbol = ("USD");
 
-    };
-    
-    if (currencyElement.value === ("CHF")) {
-        sum = (changeElement.value / 4.60);
-        exchange = (4.60);
-        symbol = ("CHF");
+//     };
 
-    };
+//     if (currencyElement.value === ("CHF")) {
+//         sum = (changeElement.value / 4.60);
+//         exchange = (4.60);
+//         symbol = ("CHF");
 
-    if (currencyElement.value === ("UAH")) {
-        sum = (changeElement.value * 11.01);
-        exchange = (11.01);
-        symbol = ("UAH");
+//     };
 
-    };
+//     if (currencyElement.value === ("UAH")) {
+//         sum = (changeElement.value * 11.01);
+//         exchange = (11.01);
+//         symbol = ("UAH");
 
-    sumElement.innerText = (sum.toFixed(2)+ symbol);
-    exchangeElement.innerText = (exchange);
-});
-}
+//     };
+
+//     sumElement.innerText = (sum.toFixed(2)+ symbol);
+//     exchangeElement.innerText = (exchange);
+// });
+// }
 
 // ciemny motyw
 
 {
-const button = document.querySelector(".button");
-const body = document.querySelector(".background");
-const themeName = document.querySelector(".themeName");
-const form__button = document.querySelector(".form__button");
-const article__link = document.querySelector(".article__link");
-const form__strong = document.querySelector(".form__strong");
+    const button = document.querySelector(".button");
+    const body = document.querySelector(".background");
+    const themeName = document.querySelector(".themeName");
+    const form__button = document.querySelector(".form__button");
+    const article__link = document.querySelector(".article__link");
+    const form__strong = document.querySelector(".form__strong");
 
 
 
 
-button.addEventListener("click", () => {
-    body.classList.toggle("dark")
+    button.addEventListener("click", () => {
+        body.classList.toggle("dark")
 
-    if (body.classList.contains("dark")){
-        themeName.innerText = "Jasny motyw";
-    } else {
-        themeName.innerText = "Ciemny motyw";
+        if (body.classList.contains("dark")) {
+            themeName.innerText = "Jasny motyw";
+        } else {
+            themeName.innerText = "Ciemny motyw";
+        };
+
+        if (body.classList.contains("dark")) {
+            button.classList = "buttonDark";
+        } else {
+            button.classList = "button";
+        };
+
+        if (body.classList.contains("dark")) {
+            form__button.classList = "form__button--dark"
+        } else {
+            form__button.classList = "form__button"
+        };
+
+        // Tu zaczyna się problem z modyfikatorami, mianowicie zmienia się klasa tylko jednego elementu "SUMA" i "DOLAR" Nie wiem w jaki sposób zrobić tak aby zmieniała się klasa w każdym z elementów.
+
+        if (body.classList.contains("dark")) {
+            article__link.classList = "article__link--dark";
+        } else {
+            article__link.classList = "article__link";
+        };
+
+        if (body.classList.contains("dark")) {
+            form__strong.classList = "form__strong--dark";
+        } else {
+            form__strong.classList = "form__strong";
+        };
+
+    });
+
+}
+
+// Własna funkcja.
+{
+    const formElement = document.querySelector(".js-form");
+    const changeElement = document.querySelector(".js-change");
+    const currencyElement = document.querySelector(".js-currency")
+    const sumElement = document.querySelector(".js-sum");
+    const exchangeElement = document.querySelector(".js-exchange");
+
+    const calculator = (amount, currency) => {
+
+        const rateEUR = 4.28;
+        const rateGBP = 4.99;
+        const rateUSD = 3.76;
+        const rateCHF = 4.60;
+        const rateUHA = 11.01;
+
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
+
+            case "GBP":
+                return amount / rateGBP;
+
+            case "USD":
+                return amount / rateUSD;
+
+            case "CHF":
+                return amount / rateCHF;
+
+            case "UHA":
+                return amount * rateUHA;
+
+        }
     };
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    if (body.classList.contains("dark")){
-        button.classList = "buttonDark";
-    } else {
-        button.classList = "button";
-    };
+        const amount = +changeElement.value;
+        const currency = currencyElement.value;
 
-    if (body.classList.contains("dark")){
-        form__button.classList = "form__button--dark"
-    } else {
-        form__button.classList = "form__button"
-    };
+        const result = calculator(amount, currency);
+        sumElement.innerText = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
+    });
 
-    // Tu zaczyna się problem z modyfikatorami, mianowicie zmienia się klasa tylko jednego elementu "SUMA" i "DOLAR" Nie wiem w jaki sposób zrobić tak aby zmieniała się klasa w każdym z elementów.
-
-    if (body.classList.contains("dark")){
-        article__link.classList = "article__link--dark";
-    } else {
-        article__link.classList = "article__link";
-    };
-
-    if (body.classList.contains("dark")){
-        form__strong.classList = "form__strong--dark";
-    } else {
-        form__strong.classList = "form__strong";
-    };
-
-});
-
+    
 }
